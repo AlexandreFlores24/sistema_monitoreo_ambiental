9f0c5fc9272717cab54e1d28696d9d4ef475df93
Valeria-SC <76540342@continental.edu.pe>
2025-11-03 03:50:18 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,6 +2,28 @@
 using namespace std;
 /*Sistema de Monitoreo Ambiental con Red de Sensores*/
 
+#define MAX 10
+int cola[MAX];
+int frente=-1;
+int final=-1; 
+
+bool estaVacia(){
+	return frente==1;
+}
+
+bool estaLlena(){
+	return final==MAX-1
+}
+
+void encolar(int dato){
+	if(estaLlena()){
+		caout<<"La cola está llena. No se puede insertar mas elementos."<<endl;
+	}else{
+		if(frente==-1){//Si la cola está vacia
+			frente=0;
+		}
+	}
+}
 int main(int argc, char *argv[])
 {
     system("PAUSE");
